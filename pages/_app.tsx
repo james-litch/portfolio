89c18a90f3b65d6b76a66b29f9f2d0383d7dc79d
James Litchfield <james.litchfield99@gmail.com>
2021-07-29 20:40:00 +0100

    use layout in app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,14 @@
 import { ChakraProvider } from '@chakra-ui/react'
 import { AppProps } from 'next/app'
+import Layout from '../Components/Layout'
 import theme from '../theme'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider theme={theme} resetCSS>
-      <Component {...pageProps} />
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
     </ChakraProvider>
   )
 }
