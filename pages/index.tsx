c71b1b765989c9b7c7ed61d9c83ca23f37872031
James Litchfield <james.litchfield99@gmail.com>
2021-07-29 22:00:53 +0100

    loremify strings

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,25 +6,25 @@ import { CgWebsite } from 'react-icons/cg'
 import { BiServer, BiCog } from 'react-icons/bi'
 
 const skills = [
-  { icon: IoPhonePortraitOutline, title: 'Mobile Apps', description: 'Professional development of applications for iOS and Android.' },
-  { icon: CgWebsite, title: 'Web Development', description: 'High-quality development of sites at the professional level.' },
-  { icon: BiServer, title: 'API Development', description: 'Professional development of applications for iOS and Android.' },
-  { icon: BiCog, title: 'GitHub Integrations', description: 'High-quality development of sites at the professional level.' }
+  { icon: IoPhonePortraitOutline, title: 'Mobile Apps', description: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit.' },
+  { icon: CgWebsite, title: 'Web Development', description: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit.' },
+  { icon: BiServer, title: 'API Development', description: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit.' },
+  { icon: BiCog, title: 'GitHub Integrations', description: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit.' }
 ]
 
 const IndexPage = () => (
   <Page title="About Me">
 
     <Box>
-      <Text as="p" marginBottom={2}>I&apos;m Creative Director and UI/UX Designer from Sydney, Australia, working in web development and print media. I enjoy turning complex problems into simple, beautiful and intuitive designs.</Text>
-      <Text as="p">My job is to build your website so that it is functional and user-friendly but at the same time attractive. Moreover, I add personal touch to your product and make sure that is eye-catching and easy to use. My aim is to bring across your message and identity in the most creative way. I created web design for many famous brand companies.</Text>
+      <Text as="p" marginBottom={2}>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptas doloribus nisi placeat. Illum nesciunt nemo ratione, temporibus voluptatem non voluptatibus!</Text>
+      <Text as="p">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsum ipsam maxime itaque maiores possimus labore vero nam. Dolorum cumque ducimus facere totam dolor exercitationem, pariatur placeat deleniti cum odio odit illo ut blanditiis voluptatum doloribus. Delectus voluptatem ipsa quis adipisci!</Text>
     </Box>
 
     <Heading as="h2" size="lg">
       What I&apos;m doing now
     </Heading>
 
-    <SimpleGrid columns={[1, 1, 2]} gap={8}>
+    <SimpleGrid columns={[1, 1, 1, 2]} gap={8}>
     {skills.map(({ icon, title, description }) => (
       <SkillCard icon={icon} title={title} description={description} key={title}/>
     ))}
