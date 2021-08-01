888c429353be7da23f87a25529ea48bc01ad379b
James Litchfield <james.litchfield99@gmail.com>
2021-08-01 13:11:07 +0100

    add meta data

--- Components/Layout.tsx
+++ Components/Layout.tsx
@@ -1,6 +1,7 @@
 // components/layout.js
 
 import { Box, Flex, useColorModeValue } from '@chakra-ui/react'
+import Head from 'next/head'
 import React, { ReactNode } from 'react'
 import { InfoCard } from './InfoCard'
 import { NavBar } from './NavBar'
@@ -8,21 +9,27 @@ import { NavBar } from './NavBar'
 export default function Layout({ children }: {children: ReactNode}) {
   const background = useColorModeValue('light.page', 'dark.page')
   return (
-    <main>
-      <Box padding={[5, 5, 5]}
-        width={['full', 'full', 'full', 1200]}
-        marginX="auto">
+    <>
+      <Head>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta name="description" content="Portfolio Website"></meta>
+      </Head>
+      <main>
+        <Box padding={[5, 5, 5]}
+          width={['full', 'full', 'full', 1200]}
+          marginX="auto">
 
-        <NavBar />
+          <NavBar />
 
-        <Flex
-          bgColor={background}
-          borderRadius="2xl"
-          flexDir={['column-reverse', 'column-reverse', 'row']}>
-            <InfoCard />
-            {children}
-        </Flex>
-      </Box>
-    </main>
+          <Flex
+            bgColor={background}
+            borderRadius="2xl"
+            flexDir={['column-reverse', 'column-reverse', 'row']}>
+              <InfoCard />
+              {children}
+          </Flex>
+        </Box>
+      </main>
+    </>
   )
 }
