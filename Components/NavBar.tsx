d6c847708ae4683762ab4ed334068d55c2ec682f
James Litchfield <james.litchfield99@gmail.com>
2021-08-01 13:12:08 +0100

    make menu more automatic

--- Components/NavBar.tsx
+++ Components/NavBar.tsx
@@ -1,21 +1,37 @@
-import { Button, Flex, HStack, useColorMode } from '@chakra-ui/react'
+import { Button, HStack, IconButton, useColorMode, useColorModeValue } from '@chakra-ui/react'
+import { BiSun, BiMoon } from 'react-icons/bi'
 import Link from 'next/link'
+import React from 'react'
+import { useRouter } from 'next/dist/client/router'
+
+const menuItems = [
+  { href: '/', label: 'Home' },
+  { href: '/resume', label: 'Resume' },
+  { href: '/portfolio', label: 'Portfolio' }
+]
 
 /**
  * NavBar Library Component
  */
 export const NavBar = () => {
-  const { colorMode, toggleColorMode } = useColorMode()
+  const { toggleColorMode } = useColorMode()
+  const router = useRouter()
+  const accentColor = useColorModeValue('light.accent', 'dark.accent')
+  const icon = useColorModeValue(<BiSun />, <BiMoon />)
+
   return (
-<Flex padding={1} justifyContent="flex-end" height="10vh">
-    <HStack>
-    <Link href="/"><Button as="a">About</Button></Link>
-    <Link href="/resume"><Button as="a">Resume</Button></Link>
-    <Link href="/portfolio"><Button as="a">Portfolio</Button></Link>
-    <Button onClick={toggleColorMode}>
-        Toggle {colorMode === 'light' ? 'Dark' : 'Light'}
-    </Button>
+    <HStack width="fit-content" marginLeft="auto" padding={4} paddingRight={1}>
+      {menuItems.map(({ href, label }) => (
+        <Link key={href} href={href} >
+          <Button
+            as="a"
+            isActive={router.pathname === href}
+            _active={{ backgroundColor: accentColor, color: 'white' }}>
+            {label}
+          </Button>
+        </Link>
+      ))}
+      <IconButton onClick={toggleColorMode} aria-label="Dark Mode Toggle" icon={icon}/>
     </HStack>
-</Flex>
   )
 }
