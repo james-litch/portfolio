22f16b3ccc760a66294f0ad0df9ee942f389b168
James Litchfield <james.litchfield99@gmail.com>
2021-08-01 19:24:49 +0100

    add in education descriptions

--- data/education.ts
+++ data/education.ts
@@ -1,7 +1,7 @@
 import { TimelineItem } from '../Components/Timeline'
 
 export const education: TimelineItem[] = [
-  { label: 'University of Liverpool', range: 'September 2017 - June 2020', description: 'Ratione voluptatem sequi nesciunt, facere quisquams facere menda ossimus, omnis voluptas assumenda est omnis..' },
-  { label: 'Carmel College', range: 'September 2015 - June 2017', description: 'Ratione voluptatem sequi nesciunt, facere quisquams facere menda ossimus, omnis voluptas assumenda est omnis..' },
-  { label: 'Lord Derby Academy', range: 'September 2011 - June 2015', description: 'Ratione voluptatem sequi nesciunt, facere quisquams facere menda ossimus, omnis voluptas assumenda est omnis..' }
+  { label: 'University of Liverpool', range: 'September 2017 - June 2020', description: 'Achieved a first class degree while studying modules such as Computer Vision and Advanced Artificial Intelligence.' },
+  { label: 'Carmel College', range: 'September 2015 - June 2017', description: 'Achieved an A* in Mathematics, B in Further Mathematics and a B in Chemistry.' },
+  { label: 'Lord Derby Academy', range: 'September 2011 - June 2015', description: 'Achieved an A*, 5A\'s, 4B\'s and a C including Mathematics and English.' }
 ]
