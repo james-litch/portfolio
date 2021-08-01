4dee83282adb32d26991afcc65680fcebe4edf59
James Litchfield <james.litchfield99@gmail.com>
2021-08-01 19:57:07 +0100

    fix typos

--- data/jobs.ts
+++ data/jobs.ts
@@ -1,7 +1,19 @@
 import { TimelineItem } from '../Components/Timeline'
 
 export const jobs: TimelineItem[] = [
-  { label: 'Dexerto (Mid-level Web Developer)', range: 'January 2021 - Present', description: 'Ratione voluptatem sequi nesciunt, facere quisquams facere menda ossimus, omnis voluptas assumenda est omnis..' },
-  { label: 'Eledev Digital (Full-stack Developer)', range: 'May 2020 - January 2021', description: 'Ratione voluptatem sequi nesciunt, facere quisquams facere menda ossimus, omnis voluptas assumenda est omnis..' },
-  { label: 'Kmatic (Junior Devloper)', range: 'May 2020 - July 2020', description: 'Ratione voluptatem sequi nesciunt, facere quisquams facere menda ossimus, omnis voluptas assumenda est omnis..' }
+  {
+    label: 'Dexerto (Mid-level Web Developer)',
+    range: 'January 2021 - Present',
+    description: 'Maintaining and improving core web vitals on dexerto.com (PHP), as well as developing components for mystats.com (Next.js).'
+  },
+  {
+    label: 'Eledev Digital (Full-stack Developer)',
+    range: 'May 2020 - January 2021',
+    description: 'Responsibilities included developing a cross platform e-commerce app, co-developing Hour Blocks: Day Planner (SwiftUI) and developing the PWS 2021 API (Nest.js).'
+  },
+  {
+    label: 'Kmatic (Junior Devloper)',
+    range: 'May 2020 - July 2020',
+    description: 'Adding content to the KMatic app (Xamarin) and developing tools to help process incoming data (C#).'
+  }
 ]
