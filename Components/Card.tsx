b781ad79e4094a4b78243ac7c565e9063d0c8612
James Litchfield <james.litchfield99@gmail.com>
2021-08-01 13:09:29 +0100

    remove card

--- Components/Card.tsx
+++
@@ -1,8 +0,0 @@
-import { Box, useStyleConfig } from '@chakra-ui/react'
-
-export const Card = (props: any) => {
-  const { variant, children, ...rest } = props
-  const styles = useStyleConfig('Card', { variant })
-  // Pass the computed styles into the `__css` prop
-  return <Box __css={styles} {...rest} >{children}</Box>
-}
