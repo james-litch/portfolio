91ab0aaea8bfab9237fa5c1151e71a7f4a550ba1
James Litchfield <james.litchfield99@gmail.com>
2021-08-01 13:10:22 +0100

    simplify skill card html

--- Components/SkillCard.tsx
+++ Components/SkillCard.tsx
@@ -1,7 +1,4 @@
-import { As, Flex, Heading, Icon, Text, useColorModeValue } from '@chakra-ui/react'
-import React from 'react'
-import { Card } from './Card'
-
+import { As, Flex, Heading, Icon, Text, useColorModeValue, VStack } from '@chakra-ui/react'
 export interface SkillCardProps {
    icon?: As<any>;
    title: string;
@@ -14,29 +11,25 @@ export interface SkillCardProps {
 export const SkillCard = ({ icon, title, description }: SkillCardProps) => {
   const titleColor = useColorModeValue('light.title', 'dark.title')
   const backgroundColor = useColorModeValue('light.card', 'dark.card')
-  const borderColor = useColorModeValue('light.card', 'dark.card')
   const iconColor = useColorModeValue('light.accent', 'dark.accent')
+
   return (
-<Card
-    backgroundColor={backgroundColor}
-    borderStyle="solid"
-    borderColor={borderColor}
-    padding={5}
-    boxShadow="md"
-    justifyContent="space-evenly">
     <Flex
-        flexDir={['column', 'column', 'row']}
-        alignItems={['center', 'center', 'flex-start']}
-        textAlign={['center', 'center', 'start']}>
+      backgroundColor={backgroundColor}
+      padding={5}
+      boxShadow="md"
+      justifyContent="space-evenly"
+      borderRadius="2xl"
+      flexDir={['column', 'column', 'column', 'row']}
+      alignItems={['center', 'center', 'center', 'flex-start']}
+      textAlign={['center', 'center', 'center', 'start']}>
 
         <Icon as={icon} boxSize={8} color={iconColor} marginBottom={5}/>
 
-        <Flex flexDir="column" marginLeft={4}>
+        <VStack align={['center', 'center', 'center', 'flex-start']} marginLeft={4}>
             <Heading size="md" color={titleColor}>{title}</Heading>
             <Text>{description}</Text>
-        </Flex>
-
+        </VStack>
     </Flex>
-</Card>
   )
 }
