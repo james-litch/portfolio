ac1dbedb77a01dce92680e046641d60342f7abc5
James Litchfield <james.litchfield99@gmail.com>
2021-08-09 18:11:39 +0100

    add in cartpole and ecommerce project

--- data/projects.ts
+++ data/projects.ts
@@ -1,8 +1,9 @@
 import { CgList } from 'react-icons/cg'
 import { IoNewspaperOutline, IoSchoolOutline } from 'react-icons/io5'
-import { GiJoystick } from 'react-icons/gi'
+import { GiBoxingRing, GiJoystick } from 'react-icons/gi'
 import { As } from '@chakra-ui/react'
 import { FaSchool } from 'react-icons/fa'
+import { SiShopify } from 'react-icons/si'
 
 export type Project = {
     icon: As<any>;
@@ -41,7 +42,7 @@ export const projects: Project[] = [
     iconBackground: 'rgba(36, 32, 32, 0.9)'
   },
   {
-    icon: GiJoystick,
+    icon: GiBoxingRing,
     title: 'PWS 2021',
     description: 'A native IOS simulator app that allows you to manage your own professional wrestling roster.',
     tags: ['SwiftUI', 'Node.js'],
@@ -49,11 +50,26 @@ export const projects: Project[] = [
     iconBackground: 'rgba(217, 59, 69, 0.9)'
   },
   {
+    icon: SiShopify,
+    title: 'Flutter E-commerce App',
+    description: 'A cross platform e-commerce app, using the Shopify GraphQL API, developed for an online clothing company.',
+    tags: ['Flutter', 'GraphQL', 'Shopify'],
+    iconBackground: 'rgba(232, 135, 180, 1)'
+  },
+  {
     icon: FaSchool,
-    title: 'University Timetable',
+    title: 'Android University Timetable',
     description: 'An Android timetable app created for the univerity\'s group software project. This improved on the existing app by adding attendance registering and location details.',
     tags: ['Java'],
     github: 'https://github.com/james-litch/UoLTimetable',
     iconBackground: 'rgba(46, 56, 166, 0.9)'
+  },
+  {
+    icon: GiJoystick,
+    title: 'Cartpole AI Agent',
+    description: 'An artificial intelligence agent, using deep reinforcement learning, created to play CartPole.',
+    tags: ['Python'],
+    github: 'https://github.com/james-litch/CartPole',
+    iconBackground: 'rgba(43, 103, 105, 1)'
   }
 ]
